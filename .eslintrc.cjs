3f7849261683bd624d03a46729c1ba5b88cc1753
parienko_sergej <parienko_sergej@enot.work>
2023-11-25 15:08:59 +0300

    stash

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,16 +3,26 @@ module.exports = {
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
     'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+    'prettier',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  plugins: [
+    'react-refresh',
+    'react',
+    '@typescript-eslint',
+    'import',
+    'jsx-a11y',
+    'react-hooks',
+    'prettier',
+  ],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'quotes': ['error', 'single'],
+    "no-duplicate-imports": "error",
   },
-}
+};
